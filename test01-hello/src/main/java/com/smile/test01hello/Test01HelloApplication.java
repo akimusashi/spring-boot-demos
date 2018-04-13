9e74a741bda528d50025901a1dab4486f57b422a
akimusashi <akimusashi@outlook.com>
2018-04-13 20:13:05 +0800

    Initial commit

---
+++ test01-hello/src/main/java/com/smile/test01hello/Test01HelloApplication.java
@@ -0,0 +1,12 @@
+package com.smile.test01hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test01HelloApplication.class, args);
+    }
+}
