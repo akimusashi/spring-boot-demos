a1153248ca44411ce51008f7e2d56fcd0150c827
akimusashi <akimusashi@outlook.com>
2018-04-18 16:06:53 +0800

    update

--- angular-test01/src/main/java/com/moon/angulartest01/controller/IndexController.java
+++ angular-test01/src/main/java/com/moon/angulartest01/controller/IndexController.java
@@ -11,4 +11,24 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/test01")
+    public String test01() {
+        return "angular-test/test01";
+    }
+
+    @RequestMapping("/controller-test")
+    public String controllerTest() {
+        return "angular-test/controller-test";
+    }
+
+    @RequestMapping("/filter-test")
+    public String filterTest() {
+        return "angular-test/filter-test";
+    }
+
+    @RequestMapping("/service-test")
+    public String serviceTest() {
+        return "angular-test/service-test";
+    }
 }
